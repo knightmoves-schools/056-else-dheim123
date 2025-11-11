0a6f869d3ec4481f6cd904a919fe224bca31dee6
dheim123 <dheim@midland.k12.ia.us>
2025-11-11 11:49:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 function calculateAward (points) {
   if (points > 99) {
-    return First Place
+    return "First Place";
+  } else if (points > 49 && points < 100) {
+    return "Second Place";
   } else {
-  return Participation Award
-}
-  
-  if (points > 49 && points < 100) {
-    return Second Place
+    return "Participation Award";
   }
-} else {
-  return Participation Award
 }
+
 
